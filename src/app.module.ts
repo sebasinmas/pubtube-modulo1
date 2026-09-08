4841478b24dfe188d7c29244c81d07c7a1d462c0
rz-eizy <duartealessandroxd@gmail.com>
2026-09-07 23:38:10 -0300

    feat: Implementacion clases services para cambiar el estado de 'programado' a 'publicado'.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { ScheduleModule } from '@nestjs/schedule';
+import { HttpModule } from '@nestjs/axios';
+import { VideoWorkerService } from './videos/video-worker.service.js';
+import { YoutubeService } from './videos/youtube.service.js';
 
 @Module({
-  imports: [],
+  imports: [
+    ScheduleModule.forRoot(),
+    HttpModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    VideoWorkerService,
+    {provide: 'YOUTUBE_SERVICE', useClass: YoutubeService}
+  ],
 })
 export class AppModule {}
