1be6a52c4f99eee359ef8090ad9d1a657a4a9c67
rz-eizy <duartealessandroxd@gmail.com>
2026-09-09 01:58:21 -0300

    chore: Se movieron los archivos de minIO a una nueva carpeta.

--- src/minio/minio.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MinioService } from './minio.service.js';
-
-@Module({
-  providers: [MinioService],
-  exports: [MinioService],
-})
-export class MinioModule {}
