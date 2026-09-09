1120219e5de4c4e0f8e6c509731ad3e26dbfe32b
rz-eizy <duartealessandroxd@gmail.com>
2026-09-09 02:07:30 -0300

    feat: Implementacion Drizzle, modificaciones en schema.ts, generacion database.module y types.ts

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -14,10 +14,15 @@ export const videoStatusEnum = pgEnum('video_status', [
   'publicado',
 ]);
 
+export type VideoStatusValue = (typeof videoStatusEnum.enumValues)[number];
+
 export const videos = pgTable('videos', {
   id: uuid('id').defaultRandom().primaryKey(),
   status: videoStatusEnum('status').default('borrador').notNull(),
   metadata: jsonb('metadata'),
   scheduled_at: timestamp('scheduled_at', { mode: 'date' }),
   youtube_url: varchar('youtube_url', { length: 255 }),
+  filename: varchar('filename', { length: 255 }).notNull(),
+  object_key: varchar('object_key', { length: 512 }).notNull(),
+  minio_upload_id: varchar('minio_upload_id', { length: 255 }).notNull(),
 });
